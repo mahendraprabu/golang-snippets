7561ed2755aa302741aec12b625013caf8f7888e
mahendraprabu <mahendra.prabu@gmail.com>
2018-02-05 17:23:15 +0530

    Added date format usage detail to date.go

--- 000-date/date.go
+++ 000-date/date.go
@@ -7,6 +7,7 @@ import (
 
 func main() {
 	startTime := time.Now()
-	//Print time in format yyyymmdd_hhmmss.msec
+	//Print time in yyyymmdd_hhmmss.msec format
+	//mm-01(month), dd-02, hh-03(15), mm-04, ss-05, yy-06 (yyyy-2006)
 	fmt.Println(startTime.Format("20060102_150405.000"))
 }
